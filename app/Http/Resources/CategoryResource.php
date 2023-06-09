3aba334a3ec89cd8ac6ba4163c5c1b21ac1d304e
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2023-06-09 18:01:20 +0100

    feat: categories for properties

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**   @mixin \App\Models\Category */
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => strval($this->id),
+            'type' => 'category',
+            'attributes' => [
+                'title' => $this->title,
+            ],
+            // 'relationships' => [
+            //     'product' => PropertyResource::collection($this->products)
+            // ]
+        ];
+    }
+}
