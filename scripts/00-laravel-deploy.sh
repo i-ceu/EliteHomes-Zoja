e9a577e6c1ccfde552bb089aa34d666a3f77326e
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2023-06-09 14:40:59 +0100

    modified:   scripts/00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer update
-composer install --no-dev
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
