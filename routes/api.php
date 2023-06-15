e26cf681d3d770d3f50e19c15345d1019e3eca69
blacczen <adeyinkaryusuf@gmail.com>
2023-06-15 10:10:47 +0100

    chore: fix route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\{
     AuthController,
-    BookingController,
     UserController,
     PropertyController,
     CategoryController,
@@ -27,6 +26,7 @@ use App\Http\Middleware\{AdminMiddleware, CheckOwnerShipMiddleware, CheckPropert
 
 Route::prefix('v1')->group(function () {
 
+
     /// Declare the heartbeat route for the API
     Route::any('/', function () {
         return response()->json(['message' => 'Welcome to Elite Homes API'], 200);
@@ -46,16 +46,13 @@ Route::prefix('v1')->group(function () {
     Route::get('/properties/{property}', [PropertyController::class, 'show']);
 
     //All Unprotected routes should be declared here.
-    Route::post('/users/{id}', [UserController::class, 'show'])->name('no-auth-user-show');
+    Route::post('/users/{id}', [UserController::class, 'show'])->name('user.show');
     Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
 
-    //Route for user to get all properties
-    Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
-    //route for user to update a product
-    Route::get('/properties/{property}', [PropertyController::class, 'show']);
     // Route for user to store a favourite
     Route::post('/favourites', [FavouriteController::class, 'store'])->name('favourite.store');
 
+    Route::apiResource('categories', CategoryController::class);
 
     //Protected routes for authenticated users
     Route::group(['middleware'  => ['auth:api']], static function () {
@@ -68,26 +65,29 @@ Route::prefix('v1')->group(function () {
             Route::put('/properties/{property}', [PropertyController::class, 'update'])->name('properties.update');
             //route for user to delete a product
             Route::delete('/properties/{property}', [PropertyController::class, 'destroy'])->name('properties.destroy');
-            Route::get('/properties/{property}/bookings', [BookingController::class, 'showAllPropertyEnquiries'])->name('show-all-property-enquiries');
         });
 
-        Route::get('/categories', [CategoryController::class, 'index'])->name('no-admin-index');
-        Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('no-admin-show');
-
-        Route::apiResource('/booking', BookingController::class);
+        
+        Route::group(['middleware' => [FavOwner::class]], static function () {
+            S
+            Route::delete('/favourites/{favourite}', [FavouriteController::class, 'delete'])->name('favourite.delete');
+        });
+    
+        Route::group(['middleware' => [UserFav::class]], static function () {
+            Route::get('/favourites', [FavouriteController::class, 'index'])->name('favourite.index');
+        });
 
         // All Admin routes should be declared here
         Route::prefix('admin')->middleware(AdminMiddleware::class)->group(function () {
-            Route::apiResource('/categories', CategoryController::class)->name('Admin', 'Categories');
-            Route::apiResource('/users', UserController::class)->name('Admin', 'Users');
+            Route::apiResource('/users', UserController::class)->name('Admin', 'users');
         });
 
         Route::group(['prefix' => 'users'],  static function () {
             Route::get('/{id}/reviews', [UserController::class, 'reviews'])->name('users.reviews');
 
             Route::group(['middleware' => [CheckOwnerShipMiddleware::class]], static function () {
-                Route::put('/{id}', [UserController::class, 'update'])->name('user-update-self');
-                Route::delete('/{id}', [UserController::class, 'destroy'])->name('users-delete-self');
+                Route::put('/{id}', [UserController::class, 'update'])->name('user.update');
+                Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
             });
         });
     });
