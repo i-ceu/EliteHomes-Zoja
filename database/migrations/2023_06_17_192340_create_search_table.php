fa43024382ec8137e48a4ef02bba45fe2ad980b3
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2023-06-21 22:26:37 +0100

    modified:   migrationscreate_search_table.php 	deleted:    resources/views/welcome.blade.php 	modified:   routes/web.php

--- database/migrations/2023_06_17_192340_create_search_table.php
+++ database/migrations/2023_06_17_192340_create_search_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('properties', function (Blueprint $table) {
+        Schema::create('search', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('properties');
+        Schema::dropIfExists('search');
     }
 };
