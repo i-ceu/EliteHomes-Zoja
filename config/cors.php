f43438acb70efc18b41b0d09bbc7665cd86ff9af
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2023-07-31 14:37:34 +0100

    chore: added localhost to allowed origins

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,7 @@ return [
         'https://elite-homes.vercel.app',
         'https://elite-homes-zojatech.netlify.app',
         'http://127.0.0.1:5173',
+        'http://localhost:5173',
         'http://127.0.0.1:5174',
         'http://localhost:19006' .
             '*'
