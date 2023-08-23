e44b25128c6b217d7c48928299ea09b8b9614003
Isaac Ubani <32121772+ubaniIsaac@users.noreply.github.com>
2023-08-23 13:26:51 +0100

    feat: queue mail

--- app/Listeners/SendWelcomeMail.php
+++ app/Listeners/SendWelcomeMail.php
@@ -8,8 +8,11 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendWelcomeMail
+class SendWelcomeMail implements ShouldQueue
 {
+    use InteractsWithQueue;
+
+    public $afterCommit = true;
     /**
      * Create the event listener.
      */
@@ -21,6 +24,9 @@ class SendWelcomeMail
     /**
      * Handle the event.
      */
+
+    // public $delay = 6;
+
     public function handle(UserSignup $event): void
     {
         Mail::to($event->user->email)->send(new WelcomeMail($event->user));
